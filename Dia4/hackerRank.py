a059ef95981d055213269f23c4d58b4790f2e5d2
Carrillo David <MiniMrSanty@gmail.com>
2025-05-11 17:10:51 -0500

    Feat: :sparkles: Exercises from HacnkerRank

---
+++ Dia4/hackerRank.py
@@ -0,0 +1,50 @@
+############################################
+
+if __name__ == '__main__':
+    print("Hello, World!")
+
+############################################
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+if (n % 2 != 0):
+    print("Weird")
+elif (n % 2 == 0 and n > 2 and n < 5):
+    print("Not Weird")
+elif (n % 2 == 0 and n > 6 and n < 20):
+    print("Weird")
+elif (n % 2 == 0 and n > 20):
+    print("Not Weird")
+else:
+    print("Weird")
+
+############################################
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum = a  + b
+    diffe = a - b
+    product = a * b
+    
+    if (1 <= a <= 10**10 and 1 <= b <= 10**10):
+        print(sum)
+        print(diffe)
+        print(product)
+
+############################################
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    div1 = a // b
+    div2 = float(a / b)
+    
+    print(div1)
+    print(div2)
+
+############################################
+
